243e736ebf8a0f46e8a411a896f36a1fa9d08eec
Hariket Sukeshkumar Sheth <72455881+hariketsheth@users.noreply.github.com>
2021-03-07 12:28:12 +0530

    Update connection.php

--- 89Tedn/Folder/Files/dashboard/connection.php
+++ 89Tedn/Folder/Files/dashboard/connection.php
@@ -1,9 +1,9 @@
 
 <?php 
 session_start();
-$con = mysqli_connect('sql305.byetcluster.com', 'epiz_26875813', 'jyrvWYP8ZiG', 'epiz_26875813_airoli');
+$con = mysqli_connect('servername', 'username', 'password', 'dbname');
 if ($con== true){
-    $_SESSION['connectionz']="prechyk";
+    $_SESSION['connectionz']="your_own_keyword";
 }
 
 ?>
