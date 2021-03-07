de05e24cf5cfb13998ecabbd1bff3f2cd8613846
Hariket Sukeshkumar Sheth <72455881+hariketsheth@users.noreply.github.com>
2021-03-07 12:28:57 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,7 @@
 
 <?php 
 session_start();
-#$con = mysqli_connect('sql305.byetcluster.com', 'epiz_26875813', 'jyrvWYP8ZiG', 'epiz_26875813_usertable');
-$con = mysqli_connect('sql305.byetcluster.com', 'epiz_26875813', 'jyrvWYP8ZiG', 'epiz_26875813_usertable');
+$con = mysqli_connect('servername', 'username', 'password', 'dbname');
 if (!$con){
     $_SESSION['connectiondb']="false";
 }
